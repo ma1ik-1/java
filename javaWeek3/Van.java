c13a0215223a91058d49a8c71ab53a10e51e5760
ma1ik-1 <126670879+ma1ik-1@users.noreply.github.com>
2023-03-15 15:57:08 +0000

    Add files via upload

---
+++ javaWeek3/Van.java
@@ -0,0 +1,21 @@
+package javaWeek3;
+
+public class Van extends Vehicle {
+
+	private boolean isCarryingLoad;
+
+	public Van(String make, String model, String colour, int year, boolean load) {
+		super(make, model, colour, year);
+		this.isCarryingLoad = load;
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isCarryingLoad() {
+		return isCarryingLoad;
+	}
+
+	public void setCarryingLoad(boolean isCarryingLoad) {
+		this.isCarryingLoad = isCarryingLoad;
+	}
+
+}
