ccd555d8dc47162c01bd8a85448c73d55337bdf6
ma1ik-1 <126670879+ma1ik-1@users.noreply.github.com>
2023-03-17 14:04:30 +0000

    Update DoggoComp.java

--- javaWeek3/DoggoComp.java
+++ javaWeek3/DoggoComp.java
@@ -12,15 +12,43 @@ public class DoggoComp {
 		int input = scanner.nextInt();
 
 		spokenEnglish(input);
+		// result(input);
 	}
 
 	public static void spokenEnglish(int n) {
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 1; i <= 100; i++) {
 //			System.out.println(i);
 			if (i != n) {
 				// skip
-				System.out.println(i);
+				// System.out.println(i);
+				result(i);
 			}
+		}
+	}
+
+	public static void result(int n) {
+		int lastDigit = n % 10;
+
+		if (lastDigit == 1) {
+			if (n == 11) {
+				System.out.println(n + "th");
+			} else {
+				System.out.println(n + "st");
+			}
+		} else if (lastDigit == 2) {
+			if (n == 12) {
+				System.out.println(n + "th");
+			} else {
+				System.out.println(n + "nd");
+			}
+		} else if (lastDigit == 3) {
+			if (n == 13) {
+				System.out.println(n + "th");
+			} else {
+				System.out.println(n + "rd");
+			}
+		} else {
+			System.out.println(n + "th");
 		}
 	}
 
